8517f026a0381f4544c966011f845405b23a9445
nigelyegon <nigelkimutai@gmail.com>
2024-07-01 18:37:02 +0300

    init all_stringd repo

---
+++ lib/all_strings.ex
@@ -0,0 +1,18 @@
+defmodule AllStrings do
+  @moduledoc """
+  Documentation for `AllStrings`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AllStrings.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
